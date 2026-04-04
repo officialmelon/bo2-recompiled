c8184979d2df277e1ce0358878a2e6bba9dd1e21
Melon <80179069+OfficiallyMelon@users.noreply.github.com>
2026-04-04 13:16:25 +1000

    Initial Commit, Rexglue Toml

---
+++ README.md
@@ -0,0 +1,2 @@
+## Re-BO2
+### Xbox 360 - Black Ops 2 (Recompile)
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+
+// bo2_recompiled - ReXGlue Recompiled Project
+//
+// This file is yours to edit. 'rexglue migrate' will NOT overwrite it.
+
+#include "generated/bo2_recompiled_config.h"
+#include "generated/bo2_recompiled_init.h"
+
+#include "bo2_recompiled_app.h"
+
+REX_DEFINE_APP(bo2_recompiled, Bo2RecompiledApp::Create)
